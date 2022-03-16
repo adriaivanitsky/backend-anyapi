14a90ac08aa588022f03f12613f34e743bcd7a5b
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-03-15 21:20:10 -0700

    making a table of rocks

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # backend-anyapi
+
+ok.. plan
+
+seed database in setup.sql
+
+set up routes in app.js
+
+write a behavioral test in app.test.js
+
+write the function im gonna wanna test in currently unnamed controller file
+(crud)
+create, read, update, delete i can never remember wtf any acronyms mean
+
+get the test to pass!
+
+create class for model
+
+doing all this stuff one time except for writing functions and corresponding tests
+
+test
+controller
+model
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS rocks;
+
+CREATE TABLE rocks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    crystal_system TEXT NOT NULL,
+    hardness TEXT NOT NULL
+);
+
+INSERT INTO 
+    rocks (name, crystal_system, hardness)
+VALUES
+    ('pyrite', 'cubic', 6),
+    ('labradorite', 'triclinic', 6),
+    ('amethyst', 'trigonal', 7);
+
