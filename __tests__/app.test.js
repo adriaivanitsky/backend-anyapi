392673aec434447e852ec3bafcf3553c6282b0b0
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-03-15 22:09:22 -0700

    got test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,21 @@ describe('backend-anyapi routes', () => {
     pool.end();
   });
   test('should read the table', async () => {
-    const expected = {};
+    // const expected = {
+    //   name: 'flourite',
+    //   crystal_system: 'isometric',
+    //   hardness: 4,
+    // };
+    const expected = [
+      { id: '1', name: 'pyrite', crystal_system: 'cubic', hardness: '6' },
+      {
+        id: '2',
+        name: 'labradorite',
+        crystal_system: 'triclinic',
+        hardness: '6',
+      },
+      { id: '3', name: 'amethyst', crystal_system: 'trigonal', hardness: '7' },
+    ];
     const response = await request(app).get('/api/v1/rocks');
 
     expect(response.body).toEqual(expected);
